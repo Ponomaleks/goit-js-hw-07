09212ad9efc4b87d93e02f4d66b7e92a04aa44e5
Aleksandr Ponomarenko <79024231+Ponomaleks@users.noreply.github.com>
2021-07-03 22:48:40 +0300

    task 6 done

--- js/task-06.js
+++ js/task-06.js
@@ -22,3 +22,11 @@
 // #validation-input.invalid {
 //   border-color: #f44336;
 // }
+
+const inputEl = document.getElementById('validation-input');
+function onInputEnterCompare(event) { if (event.currentTarget.value.length === Number(inputEl.dataset.length)) { if (inputEl.classList.contains('invalid')) { inputEl.classList.replace('invalid', 'valid') } else { inputEl.classList.add('valid') } } else {inputEl.classList.add('invalid')} }
+
+
+
+
+inputEl.addEventListener('blur', onInputEnterCompare);
